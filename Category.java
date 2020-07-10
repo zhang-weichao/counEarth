904f33dc94be33776dd6bf0f48275de5696a3793
scj <273050241@qq.com>
2020-07-10 10:28:14 +0800

    Add files via upload

---
+++ Category.java
@@ -0,0 +1,19 @@
+﻿package com.countuniverse.countearth.bean;
+
+/**
+ * @className Category
+ * Descripttion  动植物 分类 域、界、门、纲、目、科、属、种等八个主要等级。其中种是分类所用的基要单位
+ *  Domain, Kingdom, Phylum, Class, Order, Family, Genus, Species
+ *  天体的系统分类应该是7 大类即可
+ * author john
+ * Date 2020/7/9 14:16
+ * version 0.0.1
+ */
+public class Category {
+    // 编号
+    private String id;
+    // 名称
+    private String name;
+    // 昵称
+    private String nickName;
+}
---
+++ Orbit.java
@@ -0,0 +1,30 @@
+﻿package com.countuniverse.countearth.bean;
+
+/**
+ * @className Orbit
+ * Descripttion TODO
+ * author john
+ * Date 2020/7/9 11:43
+ * version 0.0.1
+ */
+public class Orbit {
+
+    // 轨道编号
+    private String id;
+    // 轨道名称
+    private String name;
+    // 轨道几何形状
+    private String geometricShape;
+    // 轨道昵称
+    private String nickName;
+    // 轨道 运行一圈 ：秒
+    private Long orbitalPeriod;
+    // 轨道直径 米
+    private Long diameter;
+    // 每一个 90的点 在四维空间的位置
+    private Location a;
+    private Location b;
+    private Location c;
+    private Location d;
+
+}
